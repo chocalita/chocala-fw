302026f7217fdcf60f004c7dde1f926ef629b5e0
yecid <yecid.pra@gmail.com>
2018-05-13 03:37:40 -0400

    Generated classes to new "Postulaciones" business

--- App/model/database/dump/20171119-1150_updates.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `tmp_formacion` ADD COLUMN `keywords` TEXT AFTER `nombre`;
-ALTER TABLE `tmp_formacion` ADD COLUMN `areas_referencia` VARCHAR(2000) DEFAULT NULL;
-ALTER TABLE `tmp_formacion` ADD COLUMN `formaciones_referencia` VARCHAR(2000) DEFAULT NULL;
