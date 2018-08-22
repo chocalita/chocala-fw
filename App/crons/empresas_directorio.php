aae4e37d4af8fae4f783a93ad9541ab928f5cbe3
yecid <yecid.pra@gmail.com>
2018-08-21 23:53:10 -0400

    Empresas Directorio

--- App/crons/empresas_directorio.php
+++ App/crons/empresas_directorio.php
@@ -1,13 +1,6 @@
-<?php
-require_once "internal.php";
 
-//if($_REQUEST['url'] == ''){
-//    header('HTTP/1.1 301 Moved Permanently');
-//    header('Location: index.htm');
-//    exit();
-//}
+require_once "internal.php";
 
 $_REQUEST['url'] == 'recursos/paginaDirectorio/testCron';
 require_once(CHOCALA_DIR.'ChocalaRunner.php');
 ChocalaRunner::run();
-?>
