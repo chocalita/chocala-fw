bca127038c0c4d0f6c7d3b60ad8123014844c84e
yecid <yecid.pra@gmail.com>
2018-08-28 22:39:59 -0400

    Empresas Directorio

--- App/crons/empresas_directorio.php
+++ App/crons/empresas_directorio.php
@@ -1,22 +1,4 @@
 <?php
-
-$rootDir = '';
-
-$applicationDir = 'App';
-
-$frameworkDir = 'Chocala';
-
-$vendorDir = 'Vendor';
-
-if(!is_dir($rootDir)){
-    $rootDir = realpath(dirname(dirname(dirname(__FILE__)))).
-        DIRECTORY_SEPARATOR;
-}
-
-file_put_contents(dirname(__FILE__).DIRECTORY_SEPARATOR."prueba.txt", "yecid");
-
-exit();
-
 require_once "internal.php";
 
 //if($_REQUEST['url'] == ''){
@@ -27,7 +9,6 @@ require_once "internal.php";
 
 $_REQUEST['url'] = 'recursos/paginaDirectorio/testCron';
 require_once(CHOCALA_DIR.'ChocalaRunner.php');
+ChocalaRunner::run();
 
-
-//ChocalaRunner::run();
 ?>
