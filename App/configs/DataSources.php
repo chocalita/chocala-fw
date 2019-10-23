55c9f6ae2a0aa718bb2f24b99b5714ea1f07bf82
Raúl Huanca <raul.ehb@gmail.com>
2019-10-22 20:44:31 -0400

    composer

--- App/configs/DataSources.php
+++ App/configs/DataSources.php
@@ -6,16 +6,16 @@
  */
 class DataSources
 {
-    
+
     static $development = array(
         'datasource'=>  'system',
-        'host'      =>  'localhost',
-        'port'      =>  '3307',
-        'dbname'    =>  'jobs',
-        'user'      =>  'root',
-        'password'  =>  ''
+        'host'      =>  '127.0.0.1',
+        'port'      =>  '3306',
+        'dbname'    =>  'empleos.click',
+        'user'      =>  'raul',
+        'password'  =>  'inventic@$'
     );
-    
+
     static $test = array(
         'datasource'=>  'jobsterin',
         'host'      =>  'mysql.empleos.click',
@@ -24,7 +24,16 @@ class DataSources
         'user'      =>  'jobsterin',
         'password'  =>  'Jobsterin.2017.pasS'
     );
-    
+
+    static $testDOcean = array(
+        'datasource'=>  'empleos_test',
+        'host'      =>  '127.0.0.1',
+        'port'      =>  '3306',
+        'dbname'    =>  'empleos_test',
+        'user'      =>  'admin',
+        'password'  =>  'AppTics2015@#$'
+    );
+
     static $production = array(
         'datasource'=>  'jobsterin',
         'host'      =>  'mysql.empleos.click',
@@ -39,5 +48,5 @@ class DataSources
         'driver'    =>  'mysql',
         'charset'   =>  'UTF8',
     );
-    
+
 }
