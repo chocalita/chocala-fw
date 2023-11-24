3f7003012f6d1f70a3f7712e71eaa818b29dcef8
Yecid Rodriguez <yecid.pra@gmail.com>
2023-11-24 18:36:34 -0400

    Secure User class

---
+++ src/Chocala/Secure/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Chocala\Secure;
+
+interface User
+{
+
+    /**
+     * @param string $password
+     * @return boolean
+     */
+    function verifyPassword($password);
+
+    /**
+     * @return User
+     */
+    function reload();
+
+    /**
+     * @return void
+     */
+    function updateAccessFailures();
+
+    /**
+     * @return void
+     */
+    function updateAccess();
+
+}
