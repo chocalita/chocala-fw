b83f8d08fce82e22d8bc5c22447a035eafdd9c81
Yecid Rodriguez <yecid.pra@gmail.com>
2024-11-04 23:51:49 -0400

    FrontController new tests

--- tests/Chocala/Http/Control/FrontControllerTest.php
+++ tests/Chocala/Http/Control/FrontControllerTest.php
@@ -3,19 +3,29 @@
 namespace Chocala\Http\Control;
 
 use Chocala\Http\Control\Fakes\FakeDispatch;
+use Chocala\Http\Fakes\FakeRequest;
 use Chocala\Http\HttpManagement;
+use Chocala\Http\HttpMethod;
+use Chocala\Http\Request\Parts\RequestLine;
+use Chocala\Http\Response\Exceptions\HttpNotImplementedException;
+use Chocala\Http\Response\Exceptions\HttpResponseException;
+use Chocala\Http\Response\Parts\StatusCode;
+use Chocala\Http\ServerInterface;
 use PHPUnit\Framework\TestCase;
 
 class FrontControllerTest extends TestCase
 {
 
+    private ServerInterface $fakeDispatch;
+
     public function setUp()
     {
+        $this->fakeDispatch = new FakeDispatch();
     }
 
     public function test__construct()
     {
-        $frontController = new FrontController(new FakeDispatch());
+        $frontController = new FrontController($this->fakeDispatch);
         self::assertNotNull($frontController);
         self::assertIsObject($frontController);
         self::assertInstanceOf(HttpManagement::class, $frontController);
@@ -28,10 +38,36 @@ class FrontControllerTest extends TestCase
      */
     public function testRoute()
     {
-        $dispatch = new FakeDispatch();
-        $frontController = new FrontController($dispatch);
-        $this->expectNotToPerformAssertions();
+        $frontController = new FrontController($this->fakeDispatch);
         // TODO: expect result in buffer
         $frontController->route();
+    }
+
+    public function testSimpleRoute()
+    {
+        $frontController = new FrontController($this->fakeDispatch);
+        try {
+            $frontController->route();
+            $this->addToAssertionCount(1);
+        } catch (\Exception $e) {
+            $this->fail($e->getMessage());
+        }
+    }
+    public function testNotImplementedRoute()
+    {
+        $dispatch = new FakeDispatch(
+            new FakeRequest(
+                new RequestLine(HttpMethod::GET(), '/section/vx/notImplemented-resource')
+            )
+        );
+        $frontController = new FrontController($dispatch);
+        try {
+            $frontController->route();
+        } catch (HttpNotImplementedException $e) {
+            self::assertInstanceOf(HttpResponseException::class, $e);
+            self::assertEquals(StatusCode::NOT_IMPLEMENTED()->code(), $e->statusCode()->code());
+        } catch (\Exception $e) {
+            $this->fail($e->getMessage());
+        }
     }
 }
